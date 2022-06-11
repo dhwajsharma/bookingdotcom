e6c5d34419f8048c8c839f50e395c4eb36c232e2
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-06-11 08:15:20 +0530

    Built Home Page

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,14 +1,27 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
-
+import PropertyList from "../../components/propertyList/PropertyList";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties.js";
+import Footer from "../../components/footer/Footer";
 import "./Home.css";
+import Featured from "../../components/featured/Featured";
+import MailList from "../../components/mailList/MailList";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type.</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
+      </div>
     </div>
   );
 };
