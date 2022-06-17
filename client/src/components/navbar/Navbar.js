8cc69f28a66a70c7e9d164fe4c9b2122749e6873
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-06-17 07:11:00 +0530

    Fetch data from node

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">booking</span>
+        <Link
+          to="/"
+          style={{
+            color: "inherit",
+            textDecoration: "none",
+            cursor: "pointer",
+          }}
+        >
+          <span className="logo">booking</span>
+        </Link>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
